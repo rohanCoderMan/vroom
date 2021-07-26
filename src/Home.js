e16b157ee42ada0c9a7062a04e386811d1e9df83
Rohan <84845548+rohanCoderMan@users.noreply.github.com>
2021-07-26 18:36:30 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -26,15 +26,9 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
-					Source code: 
-					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
-						<GitHubIcon />
-					</IconButton>
-				</div>
-				
+			
 				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
+					<h1 style={{ fontSize: "45px" }}>Vroom</h1>
 					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
 				</div>
 
@@ -45,6 +39,12 @@ class Home extends Component {
 					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
 					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
 					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+</div>
+	<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+					Source code: 
+					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/rohanCoderMan/vroom/"}>
+						<GitHubIcon />
+					</IconButton>
 				</div>
 			</div>
 		)
